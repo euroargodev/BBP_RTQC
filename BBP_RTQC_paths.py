f351491dd2823d5e4518bd9d361a056774d721e8
grg <gdal@pml.ac.uk>
2021-10-01 11:38:17 +0100

    added cell to create dirs with plots for each test

--- BBP_RTQC_paths.py
+++ BBP_RTQC_paths.py
@@ -2,6 +2,6 @@ MAIN_DIR = '/data/datasets/Argo/Original/'
 WORKING_DIR = '/fast_scratch/BBP_RTQC/'
 PKL_DIR = WORKING_DIR + 'plots/*/'
 
-DIR_PLOTS = WORKING_DIR + './plots/'
+DIR_PLOTS = WORKING_DIR + 'plots/'
 
 
