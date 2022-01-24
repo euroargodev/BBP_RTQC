4f7830a148a18b6a0a277dffdaa0796e4417f51a
grg <gdal@pml.ac.uk>
2022-01-24 10:53:53 +0000

    added option for reading PRES_ADJUSTED: corrected bug

--- BBP_RTQC.py
+++ BBP_RTQC.py
@@ -762,7 +762,7 @@ def rd_BBP(fn_p, miss_no_float, ds_config, VERBOSE=False):
     COUNTS = ds.BETA_BACKSCATTERING700[N_PROF].values
     BBP700 = ds.BBP700[N_PROF].values
     
-    if np.all(~np.isnan(ds.PRES_ADJUSTED.values)):
+    if 'PRES_ADJUSTED' in ds.keys():    
         PRES = ds.PRES_ADJUSTED[N_PROF].values
     else:
         if VERBOSE: print('no PRES_ADJUSTED for this cycle')
