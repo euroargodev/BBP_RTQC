0e494724c7cfb58c8e0ece0aefd2b42aef6afecf
grg <gdal@pml.ac.uk>
2022-06-23 10:48:13 +0100

    commit before leaving PML

--- BBP_RTQC_paths.py
+++ BBP_RTQC_paths.py
@@ -1,4 +1,5 @@
 MAIN_DIR = '/data/datasets/Argo/Original/'
+#MAIN_DIR = '/users/modellers/gdal/scratch/network/BBP_RTQC/Data/202112-ArgoData/'
 WORKING_DIR = '/fast_scratch/BBP_RTQC/'
 PKL_DIR = WORKING_DIR + 'plots/*/'
 
